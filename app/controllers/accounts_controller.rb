c8eddeadcf580d06ab87351a0dd2a5f14867729a
Adrien Lefrancq <irdane2111@gmail.com>
2018-10-23 17:29:19 -0500

    admin done go on dashboard

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,10 @@
 class AccountsController < ApplicationController
 
   def dashboard
+    @sites = Site.all
+    @creations = Creation.all
+    @videos = Video.all
+    @articles = Article.all
   end
 
 end
