c3a9720485632054b793d3617895b067fc8ba458
Adrien Lefrancq <irdane2111@gmail.com>
2018-10-24 09:54:21 -0500

    try debug prod

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,14 @@
 class AccountsController < ApplicationController
   before_action :authenticate_user!
   def dashboard
-    @sites = Site.all
-    @creations = Creation.all
-    @videos = Video.all
-    @articles = Article.all
+    if user_signed_in?
+      @sites = Site.all
+      @creations = Creation.all
+      @videos = Video.all
+      @articles = Article.all
+    else
+      redirect_to user_signed_in_path
+    end
   end
 
 end
