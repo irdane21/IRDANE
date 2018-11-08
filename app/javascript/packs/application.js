ed4039f3baa3a454645bf0029259886157c582ec
Adrien Lefrancq <irdane2111@gmail.com>
2018-11-07 18:53:20 -0600

    anime.js WIP

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,55 @@
 import "bootstrap";
 
 var myAnimation = anime({
-  targets: ['.blue', '.green'],
+  targets: ['.I'],
   translateX: '13rem',
-  rotate: 180,
-  borderRadius: '8px',
+  rotate: 360,
   duration: 2000,
-  loop: false
-
+  loop: false,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.R-1'],
+  translateX: '16rem',
+  rotate: 360,
+  duration: 2000,
+  loop: false,
+  delay: 500,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.R-2'],
+  translateX: '16rem',
+  rotate: 360,
+  duration: 2000,
+  loop: false,
+  delay: 500,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.R-3'],
+  translateX: '22rem',
+  rotate: 360,
+  duration: 2000,
+  loop: false,
+  delay: 500,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.R-4'],
+  translateX: '17.5rem',
+  rotate: 225,
+  duration: 2000,
+  loop: false,
+  delay: 500,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.D-1'],
+  translateX: '23.5rem',
+  rotate: 360,
+  duration: 2000,
+  loop: false,
+  delay: 1000,
+  backgroundColor: '#C4C4C4',
 });
