459e6ea7e7513a60961eefd392a41941d049cf7c
Adrien Lefrancq <irdane2111@gmail.com>
2018-11-08 13:42:39 -0600

    see

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,63 +2,144 @@ import "bootstrap";
 
 var myAnimation = anime({
   targets: ['.I'],
-  translateX: '13rem',
+  translateX: '26vh',
   rotate: 360,
-  duration: 2000,
+  duration: 3000,
   loop: false,
   backgroundColor: '#C4C4C4',
 });
 var myAnimation = anime({
-  targets: ['.R1'],
-  translateX: '16rem',
+  targets: ['.R1', '.R2'],
+  translateX: '35vh',
   rotate: 360,
-  duration: 2000,
-  loop: false,
-  delay: 500,
-  backgroundColor: '#C4C4C4',
-});
-var myAnimation = anime({
-  targets: ['.R2'],
-  translateX: '16rem',
-  rotate: 360,
-  duration: 2000,
+  duration: 3000,
   loop: false,
   delay: 500,
   backgroundColor: '#C4C4C4',
 });
 var myAnimation = anime({
   targets: ['.R3'],
-  translateX: '22rem',
+  translateX: '51vh',
   rotate: 360,
-  duration: 2000,
+  duration: 3000,
   loop: false,
   delay: 500,
   backgroundColor: '#C4C4C4',
 });
 var myAnimation = anime({
   targets: ['.R4'],
-  translateX: '17.5rem',
+  translateX: '38vh',
   rotate: 225,
-  duration: 2000,
+  duration: 3000,
   loop: false,
   delay: 500,
   backgroundColor: '#C4C4C4',
 });
 var myAnimation = anime({
-  targets: ['.premier'],
-  translateX: '23.5rem',
+  targets: ['.D1', '.D2'],
+  translateX: '50vh',
   rotate: 360,
-  duration: 2000,
+  duration: 3000,
   loop: false,
-  delay: 500,
+  delay: 1000,
   backgroundColor: '#C4C4C4',
 });
 var myAnimation = anime({
-  targets: ['.putaindesamere2'],
-  translateX: '25rem',
-  rotate: 360,
-  duration: 2000,
+  targets: ['.D3'],
+  translateX: '58vh',
+  rotate: 230,
+  duration: 3000,
   loop: false,
-  delay: 500,
+  delay: 1000,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.D4'],
+  translateX: '63.5vh',
+  rotate: 360,
+  duration: 3000,
+  loop: false,
+  delay: 1000,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.D5'],
+  translateX: '60vh',
+  rotate: 315,
+  duration: 3000,
+  loop: false,
+  delay: 1000,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.A1', '.A2'],
+  translateX: '69vh',
+  rotate: 360,
+  duration: 3000,
+  loop: false,
+  delay: 1500,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.A3'],
+  translateX: '83vh',
+  rotate: 360,
+  duration: 3000,
+  loop: false,
+  delay: 1500,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.A4'],
+  translateX: '77.5vh',
+  rotate: 360,
+  duration: 3000,
+  loop: false,
+  delay: 1500,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.N1'],
+  translateX: '89vh',
+  rotate: 360,
+  duration: 3000,
+  loop: false,
+  delay: 2000,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.N2'],
+  translateX: '96.8vh',
+  rotate: 320,
+  duration: 3000,
+  loop: false,
+  delay: 2000,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.N3'],
+  translateX: '103.5vh',
+  rotate: 360,
+  duration: 3000,
+  loop: false,
+  delay: 2000,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.E1', '.E2', '.E3'],
+  translateX: '110vh',
+  rotate: 360,
+  duration: 3000,
+  loop: false,
+  delay: 2500,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.E4'],
+  translateX: '117vh',
+  rotate: 360,
+  duration: 3000,
+  loop: false,
+  delay: 2500,
   backgroundColor: '#C4C4C4',
 });
