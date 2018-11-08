57bbe63394202872a55d1cc4e1995d86751af9ba
Adrien Lefrancq <irdane2111@gmail.com>
2018-11-07 20:42:19 -0600

    fucking class problem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ var myAnimation = anime({
   backgroundColor: '#C4C4C4',
 });
 var myAnimation = anime({
-  targets: ['.R-1'],
+  targets: ['.R1'],
   translateX: '16rem',
   rotate: 360,
   duration: 2000,
@@ -18,7 +18,7 @@ var myAnimation = anime({
   backgroundColor: '#C4C4C4',
 });
 var myAnimation = anime({
-  targets: ['.R-2'],
+  targets: ['.R2'],
   translateX: '16rem',
   rotate: 360,
   duration: 2000,
@@ -27,7 +27,7 @@ var myAnimation = anime({
   backgroundColor: '#C4C4C4',
 });
 var myAnimation = anime({
-  targets: ['.R-3'],
+  targets: ['.R3'],
   translateX: '22rem',
   rotate: 360,
   duration: 2000,
@@ -36,7 +36,7 @@ var myAnimation = anime({
   backgroundColor: '#C4C4C4',
 });
 var myAnimation = anime({
-  targets: ['.R-4'],
+  targets: ['.R4'],
   translateX: '17.5rem',
   rotate: 225,
   duration: 2000,
@@ -45,11 +45,20 @@ var myAnimation = anime({
   backgroundColor: '#C4C4C4',
 });
 var myAnimation = anime({
-  targets: ['.D-1'],
+  targets: ['.premier'],
   translateX: '23.5rem',
   rotate: 360,
   duration: 2000,
   loop: false,
-  delay: 1000,
+  delay: 500,
+  backgroundColor: '#C4C4C4',
+});
+var myAnimation = anime({
+  targets: ['.putaindesamere2'],
+  translateX: '25rem',
+  rotate: 360,
+  duration: 2000,
+  loop: false,
+  delay: 500,
   backgroundColor: '#C4C4C4',
 });
