d8145a25f21aa49b7b7023bc24056c9f863043cd
Adrien Lefrancq <irdane2111@gmail.com>
2018-11-16 12:26:12 -0600

    WIP

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,12 +6,12 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new()
+    @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:error] = nil
+      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
     else
-      flash.now[:error] = 'OUPS, une erreur est survenue, le message ne peut pas être envoyé...'
+      flash.now[:error] = 'Cannot send message.'
       render :new
     end
   end
