d43af1653f9d1b1d0d8972b034a9f7c3f50239eb
Adrien Lefrancq <irdane2111@gmail.com>
2019-01-14 18:31:47 +0100

    javascript added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -160,6 +160,63 @@ var myAnimation = anime({
   backgroundColor: '#C4C4C4',
 });
 
+function debounce(func, wait = 25, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll('.rotate1')
+const sliderTitles = document.querySelectorAll('.rotate2')
+
+function checkSlide(e) {
+  sliderImages.forEach(sliderImage => {
+    // half way throught the image
+    const slideInAt = (window.scrollY + window.innerHeight);
+    // console.log("slideInAt", slideInAt)
+    // console.log("offsetTop", (sliderImage.offsetTop + 1100))
+
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + 900
+    const isHalfShown = slideInAt > (sliderImage.offsetTop + 950);
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    // console.log("HalfWayShown", isHalfShown)
+    // console.log("isNotScrolledPast", isNotScrolledPast)
+    if(isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+  })
+  sliderTitles.forEach(sliderTitle => {
+    const slideInAt = (window.scrollY + window.innerHeight);
+    console.log("slideInAt", slideInAt)
+    console.log("offsetTop", (sliderTitle.offsetTop + 3350))
+
+    const imageBottom = sliderTitle.offsetTop + 3350
+    const isHalfShown = slideInAt > (sliderTitle.offsetTop + 3350);
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    console.log("HalfWayShown", isHalfShown)
+    console.log("isNotScrolledPast", isNotScrolledPast)
+    if(isHalfShown && isNotScrolledPast) {
+      sliderTitle.classList.add('active');
+    } else {
+      sliderTitle.classList.remove('active');
+    }
+  })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+
 const pressed = [];
 const secretCode = 'irdanedash';
 window.addEventListener('keyup', (e)=> {
